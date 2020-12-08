f6a0ad773d0b25922f6e8fbea8b2b515e3e2b246
vic-1998 <victor.mplaza98@gmail.com>
2020-12-08 17:33:44 +0000

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-def uppercase(str):
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    digit = (number * -1) % 10
+    digit = digit * -1
+else:
+    digit = number % 10
+print('Last digit of', number, 'is', digit, sep=' ', end=' ')
+if digit > 5:
+    print('and is greater than 5')
+elif digit == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
