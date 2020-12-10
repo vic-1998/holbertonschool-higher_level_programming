946217a89a242c43e7b96f3ce4a4755bc7be3c52
vic-1998 <victor.mplaza98@gmail.com>
2020-12-10 18:47:10 +0000

    Write a program that imports all functions from the file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for i, j in enumerate(ops):
+        if argv[2] == j:
+            print("{} {} {} = {}".format(a, j, b, funcs[i](a, b)))
+            break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
+				
+
+
+if __name__ == "__main__":
+    main()
