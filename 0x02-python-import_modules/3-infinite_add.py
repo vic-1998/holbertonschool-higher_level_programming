6e1f2b8147329afb868634bb55ef7f4bf79e6e35
vic-1998 <victor.mplaza98@gmail.com>
2020-12-10 17:16:13 +0000

    Write a program that prints the result of the addition of all arguments.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+
+
+def main():
+    value = 0
+    for i in sys.argv[1:]:
+        value += int(i)
+    print("{:d}".format(value))
+
+
+if (__name__ == "__main__"):
+    main()
