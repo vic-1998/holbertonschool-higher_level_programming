8dea56af9af5b8e29164eaa7294e2b78696d16c2
vic-1998 <victor.mplaza98@gmail.com>
2020-12-17 23:06:49 +0000

    Write a function that returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        num1 = 0
+        num2 = 0
+        for i in my_list:
+            num1 += (i[0] * i[1])
+            num2 += i[1]
+        return (num1 / num2)
+    return 0
