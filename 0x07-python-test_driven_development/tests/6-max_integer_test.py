b7681e3e3368369b49e54700e05a09a6c07268a6
vic-1998 <victor.mplaza98@gmail.com>
2021-01-09 20:42:02 +0000

    Test.

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,13 +17,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([0, 0, 0]), 0)
         self.assertEqual(max_integer([2]), 2)
         self.assertEqual(max_integer(), None)
-        a = [200, 10, 8, 36, 14, 50]
-        self.assertEqual(max_integer(a), 200)
-        A = [1, 2, 3]
-        B = [4, 5, 6]
-        C = [7, 8, 9]
-        lists = [A, B, C]
-        self.assertEqual(max_integer(lists), [7, 8, 9])
 
     if __name__ == '__main__':
         unittest.main()
