33dfcc4236a7df6dad3039788cfdff941a537643
vic-1998 <victor.mplaza98@gmail.com>
2021-01-19 14:13:55 +0000

    Python - Inheritance.

--- 0x0A-python-inheritance/101-main.py
+++ 0x0A-python-inheritance/101-main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 add_attribute = __import__('101-add_attribute').add_attribute
 
+
 class MyClass():
     pass
+
 
 mc = MyClass()
 add_attribute(mc, "name", "John")
@@ -14,4 +16,3 @@ try:
     print(a.name)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
-
