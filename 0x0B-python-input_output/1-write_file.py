9eeb3ec31a8aedd474501ecb2a89a40bd34fdae7
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 21:47:32 +0000

    Function that appends.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""Funtion write_file"""
+
+
 def write_file(filename="", text=""):
+    """return the number of the characters written"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
