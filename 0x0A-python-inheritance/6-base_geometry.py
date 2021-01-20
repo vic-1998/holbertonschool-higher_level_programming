96a85ce3d24587866144320e4a371739dd199b53
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 00:08:08 +0000

    Class BaseGeometry (based on 5-base_geometry.py).

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Funtion BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class attribute area"""
+
+    def area(self):
+        """raise an exception call"""
+        raise Exception("area() is not implemented")
