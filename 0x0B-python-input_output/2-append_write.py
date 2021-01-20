80f56ee250dc19b83301644dd11849332a04539b
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 21:45:52 +0000

    Returns the number of characters written.

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""Funtion append_write"""
+
+
 def append_write(filename="", text=""):
+    """returns the number of characters appended"""
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
