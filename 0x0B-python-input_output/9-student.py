4a69b1201877e9e1d657375be8c4b740785ed0d8
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 22:02:43 +0000

    Class Student that defines a student.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Representation Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary"""
+        return self.__dict__
