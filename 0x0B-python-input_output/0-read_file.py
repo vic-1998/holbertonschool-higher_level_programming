8347e3db0bc36b4303678d0019e5b64553408808
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 21:45:12 +0000

    Function that reads a text file (UTF8).

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""Funtion read_file"""
+
+
 def read_file(filename=""):
+    """function that reads a text file (UTF8)"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
