52fe964bad8181a87a30182c981d7ca19158136d
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 21:57:32 +0000

    Script that adds all arguments to a Python list.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""script that adds all arguments to a Python list."""
+
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
