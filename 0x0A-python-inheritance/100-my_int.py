50600d896e4c51e6e46777c5a1049f7dc9a9a18c
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 00:40:54 +0000

    MyInt.

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,10 +10,4 @@ class MyInt(int):
         """create a new instance of the class"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
-    def __eq__(self, other):
-        """method"""
-        return int(self) != other
-
-    def __ne__(self, other):
-        """method"""
-        return int(self) == other
+    
