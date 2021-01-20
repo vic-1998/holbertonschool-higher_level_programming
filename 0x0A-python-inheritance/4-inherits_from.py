e3f57135deb035bf04c1d641d81be804cc3171b7
vic-1998 <victor.mplaza98@gmail.com>
2021-01-20 00:03:22 +0000

    Return True if object is a instance of class, otherwise False.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+"""Funtion inherits_from"""
+
+
 def inherits_from(obj, a_class):
+    """return True if object is a instance of class, otherwise False"""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
