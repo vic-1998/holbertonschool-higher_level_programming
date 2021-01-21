1194696d9bf9a1dc5100b4c0678b24c52f2cc212
vic-1998 <victor.mplaza98@gmail.com>
2021-01-21 00:20:22 +0000

    Defines a student by: (based on 9-student.py).

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Representation Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
