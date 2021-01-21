950d6fd0744bc5173079ca38b2ccbc16d4706c98
vic-1998 <victor.mplaza98@gmail.com>
2021-01-21 00:27:31 +0000

    Class Student.

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Representation Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except:
+                pass
