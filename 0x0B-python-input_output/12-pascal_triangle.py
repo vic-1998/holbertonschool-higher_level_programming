48242cc17e52cc8484bc27ddb384674b2d997a7b
vic-1998 <victor.mplaza98@gmail.com>
2021-01-21 02:44:04 +0000

    Pascal_triangle.

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Function pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """Representation pascal_triangle"""
+    if n <= 0:
+        return ([])
+
+    trian = [[1]]
+    for i in range(1, n):
+        row = [1]
+        temp = trian[i - 1]
+        for j in range(len(temp)):
+            pascal = temp[j] + temp[j + 1] if j != len(temp) - 1 else 1
+            row.append(pascal)
+
+        trian.append(row)
+
+    return trian
