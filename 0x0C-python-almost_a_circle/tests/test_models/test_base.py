4b440990b76f56985ea153a907280f42941f7c5b
vic-1998 <victor.mplaza98@gmail.com>
2021-01-26 15:49:09 +0000

    Test Base.

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,7 +17,7 @@ class BaseTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """a"""
+        """Set up for the doc tests"""
         cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
 
     def setUp(self):
@@ -25,46 +25,46 @@ class BaseTest(unittest.TestCase):
         Base._Base__nb_objects = 0
 
     def test_pep8_conformance_test_base(self):
-        """Test that tests/test_models/test_base.py conforms to PEP8."""
+        """Test pep8 that models/base.py"""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['tests/test_models/test_base.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_base(self):
-        """Test models/base.py PEP8."""
+        """Test pep8 models/base.py PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/base.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_positive_id(self):
-        """A"""
+        """test positive id"""
         base = Base(16)
         self.assertEqual(base.id, 16)
         base = Base(22)
         self.assertEqual(base.id, 22)
 
     def test_negative_id(self):
-        """A"""
+        """test negatice id"""
         base = Base(-16)
         self.assertEqual(base.id, -16)
         base = Base(-22)
         self.assertEqual(base.id, -22)
 
     def tets_none_id(self):
-        """a"""
+        """test none id"""
         base = Base()
         self.assertEqual(base.id, 1)
         base = Base()
         self.assertEqual(base.id, 2)
 
     def test_docstring(self):
-        """a"""
+        """test docstring"""
         self.assertTrue(len(Base.__doc__) >= 1)
 
     def test_to_json_string(self):
-        """a"""
+        """test to_json_string"""
         Base._Base__nb_objects = 0
         line1 = {"id": 2, "width": 5, "height": 6, "x": 7, "y": 8}
         line2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
