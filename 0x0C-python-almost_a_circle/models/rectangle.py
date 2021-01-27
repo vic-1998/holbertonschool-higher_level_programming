50f01a46377e4280969993957601e12fd3bbf084
vic-1998 <victor.mplaza98@gmail.com>
2021-01-27 14:07:46 +0000

    Fix error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -50,9 +50,9 @@ class Rectangle(Base):
     def height(self, value):
         """setter of height"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
