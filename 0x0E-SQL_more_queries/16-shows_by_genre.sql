1d89a70ff02b646445370f5cd1887fc5e0ac3309
vic-1998 <victor.mplaza98@gmail.com>
2021-03-02 22:16:46 +0000

    List shows and genres .

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,6 @@
--- lists all shows, AND all genres linked to that show,
--- the database hbtn_0d_tvshows.
-FROM 
-SELECT  tv_shows.title
-       ,tv_genres.name
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
