59f06ce1803a584e8d2edc87cae5a2c89c785224
vic-1998 <victor.mplaza98@gmail.com>
2021-03-02 19:50:11 +0000

    Cities by States .

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+ -- lists all cities contained IN the database hbtn_0d_usa.
+
+SELECT  cities.id
+       ,cities.name
+       ,states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id=states.id
+ORDER BY cities.id;
