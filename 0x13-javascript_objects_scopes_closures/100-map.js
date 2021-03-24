b54ce9f302095cabea2ae37766008e4a022795f7
vic-1998 <victor.mplaza98@gmail.com>
2021-03-24 00:19:12 +0000

    Script that imports an array and computes a new array.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
+const list = require('./100-data.js').list;
 console.log(list);
-let newList = list.map((data, index) => data * index);
+const newList = list.map((data, index) => data * index);
 console.log(newList);
