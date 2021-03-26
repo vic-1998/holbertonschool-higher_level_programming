ec7cd2a2172b636a90b894b723c8befab087b1ff
vic-1998 <victor.mplaza98@gmail.com>
2021-03-26 19:34:22 +0000

    Add files.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Representation"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
