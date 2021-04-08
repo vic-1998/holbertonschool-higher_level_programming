a696534c03864300c65f15f9917d649e915e4e05
vic-1998 <victor.mplaza98@gmail.com>
2021-04-08 18:04:01 -0500

    Find a peak.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python3
+"""You are not allowed to google anything"""
+
+
+def find_peak(txtst_of_integers):
+    """function that finds a peak in a list of unsorted integers."""
+    txt = txtst_of_integers
+    l = len(txt)
+    if l == 0:
+        return
+    m = l // 2
+    if (m == l - 1 or txt[m] >= txt[m + 1]) and (m == 0 or txt[m] >= txt[m - 1]):
+        return txt[m]
+    if m != l - 1 and txt[m + 1] > txt[m]:
+        return find_peak(txt[m + 1:])
+    return find_peak(txt[:m])
