90ad661a7993c8aa4112d7209e6e8c6d82074118
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 12:27:43 -0500

    Search API.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    """Search API"""
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    rq = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=rq)
+    try:
+        res = r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
