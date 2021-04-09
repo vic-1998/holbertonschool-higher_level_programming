cff5905c081a866b626feb2c462d40535a75ee8a
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 12:09:06 -0500

    Response header value.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL
+"""
+
+if __name__ == '__main__':
+    """Response header value"""
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
