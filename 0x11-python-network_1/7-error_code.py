d879991dd37fc3a0ecade352907b688413b2c353
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 12:09:46 -0500

    Error code #1.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+A
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
