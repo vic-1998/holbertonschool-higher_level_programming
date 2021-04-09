3cecd2f0e1577f910cfea70b727b8fdf03e13bc2
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 16:28:27 -0500

    My GitHub.

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Python script that takes your GitHub
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
