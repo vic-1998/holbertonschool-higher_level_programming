8bc6fecd41a52ca3848e2a198b4d0dc78ac6e1ec
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 18:12:55 -0500

    Time for an interview./100-github_commits.py rails rails

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Python script that takes 2 arguments
+in order to solve this challenge.
+"""
+
+if __name__ == '__main__':
+    """Time for an interview!"""
+    from sys import argv
+    import requests
+    data = requests.get('https://api.github.com/repos/{}/{}/commits'
+                        .format(argv[2], argv[1]))
+    rq = data.json()
+    for commit in rq[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
