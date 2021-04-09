02abda63dbc8cc3dc942a243193e0ed897cd455f
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 12:08:15 -0500

    POST an email.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+POST an email
+"""
+
+if __name__ == "__main__":
+    """URL and an email, sends a POST"""
+    import urllib.parse as parse
+    import urllib.request as request
+    from sys import argv
+    values = {'email': argv[2]}
+    data = parse.urlencode(values).encode('utf-8')
+    rq = request.Request(argv[1], data)
+    with request.urlopen(rq) as r:
+        print(r.read().decode('utf-8'))
