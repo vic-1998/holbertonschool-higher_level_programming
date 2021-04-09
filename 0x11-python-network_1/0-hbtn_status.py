a35611b0d5dd94b4729c7494b08b70db92ccfdbf
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 12:07:44 -0500

    What's my status?.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Script that fetches URL
+"""
+
+if __name__ == "__main__":
+    """My status"""
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
