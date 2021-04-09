db40f7ab1e7348b80bd94c55e20d1e4d229e93ed
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 12:27:20 -0500

    POST an email.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     """POST an email"""
     import requests
     from sys import argv
-    x = {'email': argv[2]}
-    r = requests.post(argv[1], data=x)
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
     print(r.text)
