5e5d0817dceff60972f40c69ae66f3d250948663
vic-1998 <victor.mplaza98@gmail.com>
2021-04-09 14:24:19 -0500

    POST an email.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Script that takes in a URL 
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
