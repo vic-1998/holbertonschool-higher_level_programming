58b5acd90324a479340991e43a55e23944d351f1
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 11:06:54 -0500

    Readme.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
