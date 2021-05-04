71100c086425d90bdb6962c6f5c0277ddcb1ad70
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 12:23:08 -0500

    Star wars movie title.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const data = process.argv[2];
+request(url + data, function (err, response, body) {
+  if (response.statusCode === 200) {
+    const filejson = JSON.parse(body);
+    console.log(filejson.title);
+  } else if (err) {
+    return console.log(err);
+  }
+});
