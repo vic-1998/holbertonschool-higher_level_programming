fc5c347b0d6f6229ee072d9d6ef191cc0acf5134
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 12:21:32 -0500

    Write me.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(file, data, 'utf-8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
