c5b8f7bd734bf10bf5a40469429ec65bce094b70
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 15:06:01 -0500

    Loripsum.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const data = process.argv[3];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(data, body, 'utf-8', function (err, file) {
+      if (err != null) {
+        console.log(err);
+      }
+    });
+  }
+});
