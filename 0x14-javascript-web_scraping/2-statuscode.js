239bdb0c64ad1f1243a251074a85755bdcc466a3
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 12:22:21 -0500

    Status code.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const data = process.argv[2];
+request(data, function (err, response) {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
