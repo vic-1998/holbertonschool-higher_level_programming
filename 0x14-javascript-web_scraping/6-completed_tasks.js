0748c52e0e79fe6586e522f38a6b58b98f433a56
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 15:19:54 -0500

    How many completed.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) throw err;
+  if (response.statusCode === 200) {
+    const data = {};
+    for (const file of JSON.parse(body)) {
+      if (file.completed === true) {
+        if (file.userId in data) { data[file.userId] += 1; } else { data[file.userId] = 1; }
+      }
+    }
+    console.log(data);
+  }
+});
