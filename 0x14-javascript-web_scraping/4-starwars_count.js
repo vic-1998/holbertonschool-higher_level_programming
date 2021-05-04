1935f3b8b4df1c4df4befe9914576c4035c5470a
vic-1998 <victor.mplaza98@gmail.com>
2021-05-04 12:36:44 -0500

    Star wars Wedge Antilles.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) throw err;
+  else if (response.statusCode === 200) {
+    let count = 0;
+    let films = JSON.parse(body).results;
+    for (let movie of films) {
+      for (let char of movie.characters) {
+        if (char.endsWith('18/')) { count++; }
+      }
+    }
+    console.log(count);
+  }
+});
